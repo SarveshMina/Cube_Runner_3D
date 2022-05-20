94da0de714676c58d08df41ef99c8635b314cca4
Sarvesh Mina <minasarvesh+git@gmail.com>
2022-05-20 16:53:48 -0700

    Added coin system in the game:
    Coin System: The coin will delete when player approches it (Collects it).
    Will be added: - The coins collected will be counted.
    	       - The coins collected will display on the Game screen.
    	       - The coins collected will be stored in a bank made
    		 locally on the user computer.
    	       - The coins stored will be used to get skins.

---
+++ CubeRunner_3D/Assets/CoinSystem/CoinSystem.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CoinSystem : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider col)
+    {
+        if (col.tag == "Coin")
+        {
+            Destroy(col.gameObject);
+        }
+    }
+}
