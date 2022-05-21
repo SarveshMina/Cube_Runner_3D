2786988c55a134c18271ae51d43cd619ca3ded4a
Sarvesh Mina <minasarvesh+git@gmail.com>
2022-05-20 21:19:36 -0700

    - Added Bank, Added Coin Collecter and UI.
    - Game: - Added Bank in the game to store coins. Coins can be collected
      	  in game by players.
    	- Collected coins will be used to but skins and bank will be the
    	  communicator and coins will be the currency.
    	- Added UI to show updated coins as they're collected.

--- CubeRunner_3D/Assets/CoinSystem/CoinSystem.cs
+++ CubeRunner_3D/Assets/CoinSystem/CoinSystem.cs
@@ -1,13 +1,25 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class CoinSystem : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public Text coinDisplayText;
+    public int currentCoins = 0;
+
     void Start()
     {
-        
+        if (PlayerPrefs.HasKey("GameCoin"))
+        {
+            currentCoins = PlayerPrefs.GetInt("GameCoin");
+        } 
+        else
+        {
+            currentCoins = 0;
+        }
+
+        coinDisplayText.text = "Coins: " + currentCoins;
     }
 
     // Update is called once per frame
@@ -21,6 +33,9 @@ public class CoinSystem : MonoBehaviour
         if (col.tag == "Coin")
         {
             Destroy(col.gameObject);
+            currentCoins++;
+            PlayerPrefs.SetInt("GameCoin", currentCoins);
+            coinDisplayText.text = "Coins: " + currentCoins;
         }
     }
 }
