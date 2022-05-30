bb083165a563bebbca78ac103964b02c8c6f1bdc
Sarvesh Mina <105467665+SarveshMina@users.noreply.github.com>
2022-05-30 16:10:05 -0700

    Added Coins to the endless runner level.
    - Game: - Added coins in the endless runner game. The problem with it is
      	  that the coins are spawing very randomly and are in too much
    	  volume (for example the coins are in volume of 100 in 1
    	  Ground Plane). This is a good idea for power up so I am
    	  commiting the project here and maybe then add the powerup
    	  later in the game using this code.

--- CubeRunner_3D/Assets/Endless_level/Scripts/GroundTile.cs
+++ CubeRunner_3D/Assets/Endless_level/Scripts/GroundTile.cs
@@ -9,6 +9,8 @@ public class GroundTile : MonoBehaviour
     {
         groundSpawner = GameObject.FindObjectOfType<GroundSpawner>();
         SpawnObstacle();
+        SpawnCoins();
+        SpawnCoins();
     }
 
     private void OnTriggerExit(Collider other)
@@ -34,4 +36,33 @@ public class GroundTile : MonoBehaviour
         // Spawn the obstacle at the position
         Instantiate(obstaclePrefab, spawnPoint.position, Quaternion.identity, transform);
     }
+
+    public GameObject coinPrefab;
+
+    void SpawnCoins ()
+    {
+        int coinToSpawn = 10;
+
+        for (int i = 0; i < coinToSpawn; i++)
+        {
+            GameObject temp = Instantiate(coinPrefab);
+            temp.transform.position = GetRandomPointInCollider(GetComponent<Collider>());
+        }
+    }
+
+    Vector3 GetRandomPointInCollider (Collider collider)
+    {
+        Vector3 point = new Vector3(Random.Range(collider.bounds.min.x, collider.bounds.max.x), 
+                                    Random.Range(collider.bounds.min.y, collider.bounds.max.y),
+                                    Random.Range(collider.bounds.min.z, collider.bounds.max.z)
+                                    );
+
+        if (point != collider.ClosestPoint(point))
+        {
+            point = GetRandomPointInCollider(collider);
+        }
+
+        point.y = 0.5f;
+        return point;
+    } 
 }
